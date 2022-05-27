5fa7d2c2420c3638f3465d01d57015c669668a41
Sangalabror Pujianto <sangalabror@gmail.com>
2022-05-27 23:36:18 +0700

    test1

--- header.php
+++ header.php
@@ -1,1 +1,34 @@
-HEADER
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Menu</button>
+
+<div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title  text-center" id="offcanvasTopLabel" >Autosave</h5>
+  </div>
+  <div class="offcanvas-body">
+    <div class="row">
+        <div class="col-2">
+            <p><a href="<?php get_home_url() ?>">Home</a></p>
+            <p class="fs-3">test</p>
+            <p class="fs-3">test</p>
+        </div>
+        <div class="col-2">
+            <p class="fs-3">test</p>
+            <p class="fs-3">test</p>
+            <p class="fs-3">test</p>
+        </div>
+    </div>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
