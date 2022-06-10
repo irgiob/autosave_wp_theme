9f80fd30e051f2a1e81fe4eb19ee6b0e57ac3086
Sangalabror Pujianto <sangalabror@gmail.com>
2022-06-10 17:51:59 +0700

    added banner

--- home.php
+++ home.php
@@ -1,3 +1,78 @@
 <?php get_header(); ?>
+<div class="container text-center my-3 m-0 p-0" id="main-banner">
+    <div class="row mx-auto my-auto justify-content-center m-0 p-0 vw-100">
+        <div id="recipeCarousel" class="carousel slide m-0 p-0" data-bs-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/31f?text=1" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 1</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/e66?text=2" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 2</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/7d2?text=3" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 3</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400?text=4" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 4</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/aba?text=5" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 5</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/fc0?text=6" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 6</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev bg-transparent w-aut my-auto" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </a>
+            <a class="carousel-control-next bg-transparent w-aut my-auto" href="#recipeCarousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </a>
+        </div>
+    </div>
+</div>
 
+<script src="<?php echo get_template_directory_uri()."/assets/js/main.js";?>"></script>
 <?php get_footer(); ?>
