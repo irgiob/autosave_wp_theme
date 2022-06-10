de9d5dd4194d3f1468b78e591b5752196018ff6f
Sangalabror Pujianto <sangalabror@gmail.com>
2022-06-10 16:28:03 +0700

    added footer

--- footer.php
+++ footer.php
@@ -1,1 +1,30 @@
+<footer class="d-flex">
+    <svg class="footerlogo" viewBox="0 0 90 90" xmlns="http://www.w3.org/2000/svg" xmlns:undefined="http://www.inkscape.org/namespaces/inkscape" xml:space="preserve" version="1.1">
+        <g transform="matrix(1 0 0 -1 -20 120)">
+            <g id="logo_short_out">
+                <path d="m79.7117,41.2061l7.57,0l-21.216,65.89l-15.702,0l-21.307,-65.89l7.665,0l5.72,17.944l0.007,0.022l2.496,7.829l0.026,0l3.405,10.572l8.718,27.372l2.151,0l10.093,-31.4l-0.007,0l4.594,-14.395l5.787,-17.944z" />
+            </g>
+            <g id="logo_short_in">
+                <path d="m51.5506,65.7178l2.433,0c1.471,0 2.663,1.193 2.663,2.664l0,12.94l7.425,0l0,-13.275c0,-1.286 -1.042,-2.329 -2.328,-2.329l-2.293,0c-1.548,0 -2.804,-1.255 -2.804,-2.804l0,-12.8l-7.425,0l0,13.276c0,1.286 1.043,2.328 2.329,2.328" />
+            </g>
+        </g>
+    </svg>
+        
+    <div class="w-100"></div>
+    <div>
+        <p>Terms of use<p>
+        <p>About Us<p>
+    </div>
+    <div>
+        <p>Instagram<p>
+        <a href="mailto:mail.autosave@gmail.com">
+        <p>Contact Us<p>
+
+        </a>
+    </div>
+    <div>
+        <p>©2022 Autosave<p>
+    </div>
+</footer>
+</body>
 </html>
