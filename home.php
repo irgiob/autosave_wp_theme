98cd3a0e617eaf4b8a23f8abc0507fce462ce5ab
Irgio Ghazy Basrewan <irgiob@gmail.com>
2022-06-12 17:28:47 +0700

    added improved styling to main home banner

--- home.php
+++ home.php
@@ -1,77 +1,35 @@
 <?php get_header(); ?>
-<div class="container text-center my-3 m-0 p-0" id="main-banner">
-    <div class="row mx-auto my-auto justify-content-center m-0 p-0 vw-100">
-        <div id="recipeCarousel" class="carousel slide m-0 p-0" data-bs-ride="carousel">
-            <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active">
-                    <div class="col-md-3">
+<div class="d-flex justify-content-evenly w-75 mx-auto my-3 fw-bold">
+    <p class="hover-underline-animation-sm">Anime</p>
+    <p class="hover-underline-animation-sm">Tech</p>
+    <p class="hover-underline-animation-sm">Games</p>
+    <p class="hover-underline-animation-sm">Zine</p>
+</div>
+<div id="main-banner" class="carousel slide mb-5" data-bs-interval="false">
+    <div class="carousel-inner" role="listbox">
+        <?php for($i=1;$i<7;$i++): ?>
+            <div class="carousel-item <?php if($i==1){ echo "active";}?>">
+                <div class="col-md-3">
+                    <a href="">
                         <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/31f?text=1" class="img-fluid">
+                            <div class="card-img overflow-hidden h-100">
+                                <img src="//via.placeholder.com/720x1280/<?php printf( "%06X\n", mt_rand( 0, 0xFFFFFF )); ?>?text=<?php echo $i;?>">
                             </div>
-                            <div class="card-img-overlay">Slide 1</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/e66?text=2" class="img-fluid">
+                            <div class="card-img-overlay">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula.</p>
                             </div>
-                            <div class="card-img-overlay">Slide 2</div>
                         </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/7d2?text=3" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 3</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400?text=4" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 4</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/aba?text=5" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 5</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-img">
-                                <img src="//via.placeholder.com/500x400/fc0?text=6" class="img-fluid">
-                            </div>
-                            <div class="card-img-overlay">Slide 6</div>
-                        </div>
-                    </div>
+                    </a>
                 </div>
             </div>
-            <a class="carousel-control-prev bg-transparent w-aut my-auto" href="#recipeCarousel" role="button" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            </a>
-            <a class="carousel-control-next bg-transparent w-aut my-auto" href="#recipeCarousel" role="button" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            </a>
-        </div>
+        <?php endfor; ?>
     </div>
+    <a class="carousel-control-prev bg-transparent" href="#main-banner" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    </a>
+    <a class="carousel-control-next bg-transparent" href="#main-banner" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    </a>
 </div>
 
 <script src="<?php echo get_template_directory_uri()."/assets/js/main.js";?>"></script>
